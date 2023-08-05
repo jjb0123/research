bf2e0273ce86e4c226a685e9de45a10113f8e735
Josh Brice <jjhbrice@gmail.com>
2023-08-05 15:36:05 +1000

    Create text_preprocessing.py

---
+++ text_preprocessing.py
@@ -0,0 +1,14 @@
+# High Level Pipeline
+
+from transformers import pipeline 
+
+pipe = pipeline("text-generation",model="distilgpt2")
+
+
+#Direct Model Load 
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer= AutoTokenizer.from_pretrained("distilgpt2")
+model=AutoModelForCausalLM.from_pretrained("distilgpt2")
+
+inputs = tokenizer("Hello my dog is cute", return_tensors="pt")
