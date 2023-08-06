c266a77e19e0c5c5f3fd093a7a22aa7b08ed8e0d
Josh Brice <jjhbrice@gmail.com>
2023-08-06 21:43:24 +1000

    Update text_preprocessing.py

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,5 +1,5 @@
 # High Level Pipeline
-
+import torch 
 from transformers import pipeline 
 
 pipe = pipeline("text-generation",model="distilgpt2")
@@ -12,3 +12,5 @@ tokenizer= AutoTokenizer.from_pretrained("distilgpt2")
 model=AutoModelForCausalLM.from_pretrained("distilgpt2")
 
 inputs = tokenizer("Hello my dog is cute", return_tensors="pt")
+
+print(inputs)
