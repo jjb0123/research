b551ec5d2ad5a5eab18aa9683b4c8e97660b4f5e
Josh Brice <jjhbrice@gmail.com>
2023-08-07 15:26:30 +1000

    Model working

--- model.py
+++ model.py
@@ -1,17 +1,18 @@
-# High Level Pipeline
-import torch 
-from transformers import pipeline 
+# # High Level Pipeline
+# import torch 
+# from transformers import pipeline 
+import pandas as pd
 
-pipe = pipeline("text-generation",model="distilgpt2")
+# pipe = pipeline("text-generation",model="distilgpt2")
 
 
-#Direct Model Load 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+# #Direct Model Load 
+# from transformers import AutoTokenizer, AutoModelForCausalLM
 
-tokenizer= AutoTokenizer.from_pretrained("distilgpt2")
-model=AutoModelForCausalLM.from_pretrained("distilgpt2")
+# tokenizer= AutoTokenizer.from_pretrained("distilgpt2")
+# model=AutoModelForCausalLM.from_pretrained("distilgpt2")
 
-inputs = tokenizer("Hello my dog is cute", return_tensors="pt")
+# inputs = tokenizer("Hello my dog is cute", return_tensors="pt")
 
 
 import requests
@@ -27,3 +28,6 @@ def query(payload):
 output = query({
 	"inputs": "Can you please let us know more details about your ",
 })
+
+print(output)
+
