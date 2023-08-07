10991381b9d117989819db450fcacf475cb0971b
Josh Brice <jjhbrice@gmail.com>
2023-08-07 14:33:50 +1000

    Scraped Datasets Added

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -13,4 +13,17 @@ model=AutoModelForCausalLM.from_pretrained("distilgpt2")
 
 inputs = tokenizer("Hello my dog is cute", return_tensors="pt")
 
-print(inputs)
+
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/distilgpt2"
+API_TOKEN="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"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
