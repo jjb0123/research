d1ac4746006f381c767667780223e93c9725a4a6
Josh Brice <jjhbrice@gmail.com>
2023-08-07 23:24:12 +1000

    Zach comments

--- model.py
+++ model.py
@@ -25,9 +25,25 @@ def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 	
+# Don't expect conversation
 output = query({
 	"inputs": "Tell me how the knight moves in chess",
 })
 
+#Instead of asking questions, prompt in a way that you want it to complete the rest of the sentence. 
+# Try to take the beginning of a review for example, or take the skeleton of a review 
+# To prompt a new review, give a data enrichment eg. "This is a 5 star review. Food was amazing"
+
+
+
+# Don't focus on the perceivable quality of output 
+
+# Come up with an automated metric to see how well the model is doing. 
+
+# Start with DistilGPT2. Doesn't have to be the only model we use. Almost infinite room for expansion
+
+# Need a different model for sentiment classification. Generating a text subject to a constraint 
+
+# Talk to Zach about Fine-Tuning DistilGPT2 
 print(output)
 
